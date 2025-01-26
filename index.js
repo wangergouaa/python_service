d48511faf3c48e3d2e85763b13f84d73a930df29
于惠亚 <wb_yuhuiya@meituan.com>
2025-01-26 21:57:25 +0800

    修改支持js文件

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, 4everland!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
